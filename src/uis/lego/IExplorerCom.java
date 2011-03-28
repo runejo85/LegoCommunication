3564aca6c62b0c68ca98dec1fe21cc79faaf5c03
Eirik Nordbø <eirik.nordboe@gmail.com>
2011-03-28 12:05:30 +0200

    zcv

--- src/uis/lego/IExplorerCom.java
+++ src/uis/lego/IExplorerCom.java
@@ -1,12 +1,6 @@
 package uis.lego;
 
-/**
- * Created by IntelliJ IDEA.
- * User: eirik
- * Date: 3/25/11
- * Time: 12:03 PM
- * To change this template use File | Settings | File Templates.
- */
+
 public interface IExplorerCom {
 
     public int[] sweep();
